48c316041cf603ee66cea6b5af5b4594a1a31e3f
azharalsharbaji <113019567+azharalsharbaji@users.noreply.github.com>
2023-07-31 19:26:20 +0300

    adding dist

---
+++ assets/VCardText.4efe58ee.js
@@ -0,0 +1,1 @@
+import{al as t}from"./index.3c7869e1.js";const e=t("v-card-text");export{e as V};
---
+++ assets/VSpacer.e4a2d732.js
@@ -0,0 +1,1 @@
+import{al as a}from"./index.3c7869e1.js";const r=a("flex-grow-1","div","VSpacer");export{r as V};
